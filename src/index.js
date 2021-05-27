b23f7ae3f550a996b2f91a939027d4f1ede1dd05
apJammanbo <apJammanbo@gmail.com>
2021-05-27 20:59:50 +0900

    react-perf-devtool 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { registerObserver } from "react-perf-devtool";
+console.log(registerObserver);
+registerObserver();
 
 ReactDOM.render(
   <React.StrictMode>
